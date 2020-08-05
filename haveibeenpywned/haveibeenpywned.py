a229393bd6873f688f03e6121b39456619a16b16
unknown <Sam@SAM-X1.noddy.mckinnonsc.vic.edu.au>
2020-08-05 14:56:45 +1000

    docstrings and return list of breach names

--- haveibeenpywned/haveibeenpywned.py
+++ haveibeenpywned/haveibeenpywned.py
@@ -22,12 +22,31 @@ class Pywned:
         self.headers["hibp-api-key"] = api_key
 
     def _do_request(self, endpoint):
+        """Internal method for building request url and performing HTTP request to the
+        HIBP API
+
+        Args:
+            endpoint (string): API endpoint string (eg. "breachedaccount" or "breach" )
+
+        Returns:
+            json: Requests response .json() object
+        """
         resp = requests.get(urljoin(API_BASE_URL, endpoint), headers=self.headers)
         resp.raise_for_status()
         return resp.json()
 
     def get_all_breaches_for_account(self, email):
-        return self._do_request(urljoin("breachedaccount", email))
+        """Returns a list of all breaches a particular account (email) has been involved
+        in
+
+        Args:
+            email (string): Account (email address) to search for breaches
+
+        Returns:
+            list: Names of breaches the email (account) has been involved in
+        """
+        resp = self._do_request(urljoin("breachedaccount", email))
+        return [breach["Name"] for breach in resp]
 
 
 if __name__ == "__main__":
