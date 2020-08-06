b170ee2fb57d4b345b5aad53795ba2b140a725a3
unknown <Sam@SAM-X1.noddy.mckinnonsc.vic.edu.au>
2020-08-06 11:58:46 +1000

    Fix #1

--- haveibeenpywned/_example_usage.py
+++ haveibeenpywned/_example_usage.py
@@ -1,5 +1,4 @@
 from haveibeenpywned import Pywned
 
-pywned = Pywned()
-pywned.api_key = "your_hibp_api_key"
+pywned = Pywned("your_hibp_api_key")
 pywned.get_all_breaches_for_account("your@email.com")
