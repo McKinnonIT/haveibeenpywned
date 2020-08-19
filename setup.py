b652d7bc6073e38aa68b10538c1fb29cdc766eab
Sam Neal <sam.w.neal@gmail.com>
2020-08-19 15:54:59 +1000

    add required properties

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="haveibeenpywned",
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="haveibeenpywned",  # Replace with your own username
     version="0.1",
-    description="A simple python wrapper for the www.haveibeenpwned.com API.",
-    url="https://github.com/scv-m/haveibeenpywned",
     author="Sam Neal",
     author_email="sam.w.neal@gmail.com",
+    description="A simple python wrapper for the www.haveibeenpwned.com API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scv-m/haveibeenpywned",
+    download_url="",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=["ratelimit", "requests",],
 )
