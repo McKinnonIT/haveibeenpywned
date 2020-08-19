70f02de713c3a73a625bbed0d1bba0a0d69fb0fe
Sam <sam.w.neal@gmail.com>
2020-08-19 16:06:15 +1000

    4 publish to pypi (#5)
    * restrict action to master branch, update readme
    
    * Update description
    
    * add required properties
    
    * add setup.cfg
    
    * add license.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="haveibeenpywned",
-    version="0.2",
-    description="A simple python wrapper for the www.haveibeenpwned.com API.",
-    url="https://github.com/scv-m/haveibeenpywned",
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="haveibeenpywned",  # Replace with your own username
+    version="0.1",
     author="Sam Neal",
     author_email="sam.w.neal@gmail.com",
+    description="A simple python wrapper for the www.haveibeenpwned.com API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scv-m/haveibeenpywned",
+    download_url="",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=["ratelimit", "requests",],
 )
