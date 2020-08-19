f0470814da4d4d909dd0a91de7fe7e6c522d0459
Sam Neal <sam.w.neal@gmail.com>
2020-08-19 16:08:01 +1000

    remove comment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="haveibeenpywned",  # Replace with your own username
+    name="haveibeenpywned",
     version="0.1",
     author="Sam Neal",
     author_email="sam.w.neal@gmail.com",
